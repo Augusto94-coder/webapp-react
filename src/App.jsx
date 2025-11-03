04220ba47b2f27e3103a3530abc2bff02d262304
Augusto94-coder <augustodileo94@gmail.com>
2025-11-03 17:51:25 +0100

    first commit + milestone 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+            <div>
+                <h1>Benvenuti nella nostra APP di film</h1>
+            </div>
+        </>
+  )
+}
+
+export default App
